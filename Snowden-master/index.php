a570387f072cae4024a692d5ff82b10b0e6c1fe9
Justin Lipkin <jl164109@truro-penwith.ac.uk>
2019-11-15 15:00:38 +0000

    Altered Index and NavBar to use includes instead and also added a base footer.php

--- Snowden-master/index.php
+++ Snowden-master/index.php
@@ -1,29 +1,48 @@
 <?php
-$con = mysqli_connect("localhost","jg_monty","123abc","jg_monty");
-
-// Check connection
-// if ($con->connect_error) {
-//     die("Connection failed: " . $con->connect_error);
-// }
-// echo "Connected successfully";
-
+    // Make Connection String
+    $mysqlConnection = @mysqli_connect("localhost", "jg_luke", "123abc", "jg_luke")
+    or die ("<h1> Sorry- Could not connect to the MySQL Database at this time. </h1> <a href='http://www.google.com'> <button><h1> Leave </h1></button> </a>");	;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Snowden</title>
-</head>
-<body style="bg-light">
-<?php require 'navbar.php'; ?>
-</body>
-<?php require 'frontpage.php' ?>
+    <head>
+        <!--- META + Title --->
+        <title>Snowden</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!--- Styles --->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body style="bg-light">
+        <!--- Header --->
+        <header>
+            <?php include('navbar.php'); ?>
+        </header>
+        <!--- Main --->
+        <main>
+            <?php
+                if(isset($_GET['location'])){
+                    include(strip_tags($_GET['location']));
+                }
+                else{
+                    include('frontpage.php');
+                }
+            ?>
+        </main>
+        <!--- Footer --->
+        <footer>
+            <?php include('footer.php'); ?>
+            <!--- Scripts (JQuery, BootStrap, Popper) --->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </footer>
+    </body>
 </html>
         
+<?php
+    // End PHP Connection
+    die();
+?>
--- Snowden-master/navbar.php
+++ Snowden-master/navbar.php
@@ -1,35 +1,35 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-                <img src="/johng/jg_monty/Snowden-master/logo.png" class="img-fluid" style="max-height: 80px;">
-            </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link " href="/johng/jg_monty/Snowden-master/index.php">Frontpage </a>
-            </li>
-            <li>
-              <a class="nav-item nav-link ml-auto text-dark" href="#">Topics</a>
-              
-            </li>
-                        
-          </ul>
-          <ul class="navbar-nav ml-auto">
-            <li>
-              <a class="nav-item nav-link text-dark" href="#">Your Stories</a>
-            </li>
-            <li class="ml-auto">
+  <a class="navbar-brand" href="?location=frontpage.php">
+          <img src="logo.png" class="img-fluid" style="max-height: 80px;">
+      </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
-              <a class="nav-item nav-link text-dark" href="/johng/jg_monty/Snowden-master/auth/account.php">Your Profile</a>
-            <?php else: ?>
-              <a class="nav-item nav-link text-dark" href="/johng/jg_monty/Snowden-master/auth/account.php">Login</a>
-            <?php endif; ?>
-            </li>
-          </ul>
-          
-        </div>
-      </nav>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link " href="?location=frontpage.php">Frontpage </a>
+      </li>
+      <li>
+        <a class="nav-item nav-link ml-auto text-dark" href="?location=topics.php">Topics</a>
+        
+      </li>
+                  
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li>
+        <a class="nav-item nav-link text-dark" href="?location=stories.php">Your Stories</a>
+      </li>
+      <li class="ml-auto">
+
+      <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
+        <a class="nav-item nav-link text-dark" href="?location=auth/account.php">Your Profile</a>
+      <?php else: ?>
+        <a class="nav-item nav-link text-dark" href="?location=auth/login.php">Login</a>
+      <?php endif; ?>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
